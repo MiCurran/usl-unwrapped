dfddde7907143d35023f5ddcfa4118165bdc939d
Micurran <mcurran1722@gmail.com>
2023-10-05 20:09:18 -0600

    turn off task service for live scores - add delete liveScores route

--- src/scores/scores.module.ts
+++ src/scores/scores.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { ScoresService } from './scores.service';
 import { ScoresController } from './scores.controller';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { ConfigService } from '@nestjs/config';
 
 @Module({
-  providers: [ScoresService, PrismaService],
+  providers: [ScoresService, PrismaService, ConfigService],
   controllers: [ScoresController]
 })
 export class ScoresModule {}
