256c8d9ede89b9b427fc55ef9971144541058193
Micurran <mcurran1722@gmail.com>
2023-10-07 17:12:57 -0600

    adding db write for live scores

--- src/scraping/scraping.module.ts
+++ src/scraping/scraping.module.ts
@@ -3,10 +3,11 @@ import { ScrapingController } from './scraping.controller';
 import { ConfigService } from '@nestjs/config';
 import { ScrapingService } from './scraping.service';
 import { ScoresService } from 'src/scores/scores.service';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Module({
   controllers: [ScrapingController],
   imports: [],
-  providers: [ConfigService, ScrapingService, ScoresService]
+  providers: [ConfigService, ScrapingService, ScoresService, PrismaService]
 })
 export class ScrapingModule {}
