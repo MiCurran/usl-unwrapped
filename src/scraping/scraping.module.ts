8d1758ef6b664664bf7448eec22c0c392dc5b4ec
Micurran <mcurran1722@gmail.com>
2023-10-07 17:07:33 -0600

    adding db write for live scores

--- src/scraping/scraping.module.ts
+++ src/scraping/scraping.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { ScrapingController } from './scraping.controller';
 import { ConfigService } from '@nestjs/config';
 import { ScrapingService } from './scraping.service';
+import { ScoresService } from 'src/scores/scores.service';
 
 @Module({
   controllers: [ScrapingController],
   imports: [],
-  providers: [ConfigService, ScrapingService]
+  providers: [ConfigService, ScrapingService, ScoresService]
 })
 export class ScrapingModule {}
